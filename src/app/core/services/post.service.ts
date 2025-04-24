417d0ab7f7621c6e87dd44817b3a71314bf25d30
khadijaDev <khadijaoukhlift@gmail.com>
2025-04-24 17:34:12 +0200

    updates

--- src/app/core/services/post.service.ts
+++ src/app/core/services/post.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
+  private apiUrl = 'http://localhost:3000/posts'; // JSON Server URL for posts
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getPosts(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  getPost(id: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
+
+  addPost(post: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, post);
+  }
+
+  updatePost(id: string, post: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/${id}`, post);
+  }
+
+  deletePost(id: string): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/${id}`);
+  }
 }
