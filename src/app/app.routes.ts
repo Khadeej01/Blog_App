0b77e89871a647fc4d18f0c79d2322133208435d
fatimaezzahraadyani <fatimaezzahraadyani@gmail.com>
2025-04-25 09:33:02 +0200

    post form add post

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PostListComponent } from './features/posts/post-list/post-list.component';
+import { PostDetailComponent } from './features/posts/post-detail/post-detail.component';
+import { PostFormComponent } from './features/posts/post-form/post-form.component';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AuthGuard } from './core/guards/auth.guard';
+import { NavbarComponent } from './features/navbar/navbar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: '', component : NavbarComponent},
+  { path: 'posts', component: PostListComponent },
+  { path: 'post/:id', component: PostDetailComponent },
+  { path: 'post-form', component: PostFormComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: '' }
+];
+
--- src/app/features/posts/post-list/post-list.component.ts
+++ src/app/features/posts/post-list/post-list.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NgFor, NgIf } from '@angular/common';
+import { PostService } from '../../../core/services/post.service';
+import { CommonModule} from '@angular/common';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-post-list',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, NgFor, NgIf],
   templateUrl: './post-list.component.html',
-  styleUrl: './post-list.component.css'
+  styleUrls: ['./post-list.component.scss']
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit {
+  posts: any[] = [];
+  errorMessage: string | null = null; // Add error message property
 
+  constructor(private postService: PostService) {}
+
+  ngOnInit(): void {
+    this.postService.getPosts().subscribe({
+      next: (posts) => {
+        this.posts = posts;
+      },
+      error: (err) => {
+        this.errorMessage = 'Erreur lors du chargement des articles : ' + (err.message || 'Erreur inconnue');
+      }
+    });
+  }
+
+  deletePost(id : number): void{
+    this.postService.deletePost(id).subscribe(()=>{
+      this.posts = this.posts.filter(post => post.id !== id );
+    });
+  }
 }
